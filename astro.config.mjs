229cfee93977047e65ee22283539321f7fdb3480
lukasz0707 <lukaszs0707@gmail.com>
2026-04-13 13:15:26 +0200

    add basic prefetch on hover

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify()
+  prefetch: true,
+  adapter: netlify(),
 });
